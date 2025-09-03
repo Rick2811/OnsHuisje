041ac4d3eac7d2e427aa5d7b9d6856bda0beade9
Rick2811 <r.schoenmaker2@student.han.nl>
2025-09-03 11:56:43 +0200

    d

--- onshuisje-vercel/src/WebsiteEntrance.js
+++ onshuisje-vercel/src/WebsiteEntrance.js
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 
 function WebsiteEntrance() {
@@ -386,4 +385,4 @@ function escapeHtml(s){return String(s).replace(/[&<>"']/g,(m)=>({ "&":"&amp;","
 // Events
 $add?.addEventListener("click", addTab);
 $search?.addEventListener("input", render);
->>>>>>> parent of 3af1a47 (update databasse via vercel)
+
