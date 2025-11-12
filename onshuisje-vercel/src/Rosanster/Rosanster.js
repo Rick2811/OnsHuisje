75f359c402b984d9230da16797f4d29f7086d5c5
Rick2811 <r.schoenmaker2@student.han.nl>
2025-11-12 22:35:30 +0100

    c

---
+++ onshuisje-vercel/src/Rosanster/Rosanster.js
@@ -0,0 +1,9 @@
+export function initRosanster() {
+    const app = document.getElementById("app");
+    if (app) {
+        app.innerHTML = `
+      <h2>Welkom bij Rosanster</h2>
+      <p>Hier staat nu de nieuwe interface.</p>
+    `;
+    }
+}
